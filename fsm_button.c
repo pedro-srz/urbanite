98ac734eb89644f5559b8f385577c3b121eabb92
josueportiz <josueportiz@gmail.com>
2025-01-23 18:00:24 +0100

    No cast in init anymore

--- fsm_button.c
+++ fsm_button.c
@@ -21,23 +21,19 @@
 /* State machine output or action functions */
 
 /* Other auxiliary functions */
-void fsm_button_init(fsm_t *p_this, uint32_t debounce_time_ms, uint32_t button_id)
+void fsm_button_init(fsm_button_t *p_fsm_button, uint32_t debounce_time, uint32_t button_id)
 {
-    // Initialize the FSM
-    fsm_init(p_this, fsm_trans_button);
+    fsm_init(&p_fsm_button->f, fsm_trans_button);
 
-    // Retrun the pointer to the FSM
-    fsm_button_t *p_fsm = (fsm_button_t *)(p_this);
-
-    /* TO-DO alumnos: */
+    /* TODO alumnos: */
 }
 
 /* Public functions -----------------------------------------------------------*/
 fsm_button_t *fsm_button_new(uint32_t debounce_time, uint32_t button_id)
 {
-    fsm_t *p_fsm = malloc(sizeof(fsm_button_t));      /* Do malloc to reserve memory of all other FSM elements, although it is interpreted as fsm_t (the first element of the structure) */
-    fsm_button_init(p_fsm, debounce_time, button_id); /* Initialize the FSM */
-    return (fsm_button_t *)(p_fsm);                   /* Composite pattern: return the fsm_t pointer as a fsm_button_t pointer */
+    fsm_button_t *p_fsm_button = malloc(sizeof(fsm_button_t)); /* Do malloc to reserve memory of all other FSM elements, although it is interpreted as fsm_t (the first element of the structure) */
+    fsm_button_init(p_fsm_button, debounce_time, button_id);   /* Initialize the FSM */
+    return p_fsm_button;                                       /* Composite pattern: return the fsm_t pointer as a fsm_button_t pointer */
 }
 
 /* FSM-interface functions. These functions are used to interact with the FSM */
@@ -48,7 +44,7 @@ void fsm_button_fire(fsm_button_t *p_fsm)
 
 void fsm_button_destroy(fsm_button_t *p_fsm)
 {
-    fsm_destroy(&p_fsm->f);
+    free(&p_fsm->f);
 }
 
 fsm_t *fsm_button_get_inner_fsm(fsm_button_t *p_fsm)
